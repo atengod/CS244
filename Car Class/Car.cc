49f1a9dda153a708a921e286e8af1e125819c0a9
atengod <32345524+atengod@users.noreply.github.com>
2018-03-12 21:31:11 -0400

    Update Car.cc

--- Car Class/Car.cc
+++ Car Class/Car.cc
@@ -1,1 +1,21 @@
-#empty
+//Car class implementation	   
+//Car.cc
+
+Car::Car()
+{
+center_x = 0;
+center_y = 0;
+}
+
+Car::Car(double x, double y, double r)
+{
+center_x = x;
+center_y = y;
+}
+
+void Car::setCenter(double dx, double dy)
+{
+center_x = dx;
+center_y = dy;
+}
+
