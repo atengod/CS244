d4b4f96718d42e031e922c990e748086033ff5ec
atengod <32345524+atengod@users.noreply.github.com>
2018-03-12 21:29:24 -0400

    Update Car.h

--- Car Class/Car.h
+++ Car Class/Car.h
@@ -1,1 +1,21 @@
-#empty
+//Car class declaration
+//Car.h
+
+class Car
+{
+private:
+  int yearModel;
+  string make;
+  int speed;	
+public:
+  Car(int year, string makee);
+  void brake();
+  void accelerate();
+  void setSpeed(int sp);
+  int getSpeed();
+  void setMake(string makee);
+  string getMake();
+  void setyearModel(int year);
+  int getyearModel();
+};
+ 
